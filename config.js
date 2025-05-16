1a10cdc4c0afc8f3d9a83631852ddfce55aa53a7
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-05-15 19:03:47 -0600

    include host

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
 // Import sequelize
 const { Sequelize } = require("sequelize");
 
+const HOST = process.env.DB_HOST;
 const DATABASE = process.env.DATABASE;
 const USER = process.env.USER;
 const PASSWORD = process.env.PASSWORD;
-const PORT = 5432; // Default PostgreSQL port
+const PORT = 5432;
 
 // Use sequelize to make a connection to the database
 const sequelize = new Sequelize(DATABASE, USER, PASSWORD, {
-  host: "localhost",
+  host: HOST,
   dialect: "postgres",
   port: PORT,
 });
