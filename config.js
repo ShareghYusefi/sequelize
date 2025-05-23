5f71be29643961b2b868d6ea5f89487e2de900c9
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-05-22 19:13:43 -0600

    alternative connection to postgres

--- config.js
+++ config.js
@@ -8,11 +8,12 @@ const PASSWORD = process.env.PASSWORD;
 const PORT = 5432;
 
 // Use sequelize to make a connection to the database
-const sequelize = new Sequelize(DATABASE, USER, PASSWORD, {
-  host: HOST,
-  dialect: "postgres",
-  port: PORT,
-});
+const sequelize = new Sequelize(
+  "postgresql://sharegh:jDZD7xUrFSLbTKyqZUdQJ7iUBjunrIqM@dpg-d0ns6vidbo4c73a7jpbg-a/organization_f5pk",
+  {
+    dialect: "postgres",
+  }
+);
 
 // Export the sequelize object
 module.exports = sequelize;
